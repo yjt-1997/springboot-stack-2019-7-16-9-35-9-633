2458aa065f8c897d63cba1b5bcbd8673c7966d55
YOUNG YANG (ITA-ISDC-ISD-OOCLL/ZHA) <YANGYO7@OOCL.COM>
2019-07-17 16:01:02 +0800

    implement

---
+++ src/main/java/com/tw/apistackbase/testEntity/Company.java
@@ -0,0 +1,39 @@
+package com.tw.apistackbase.entity;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+
+public class Company {
+    private String companyName;
+    private int employeesNumber;
+    private List<Employee> employees;
+
+    public Company() {
+    }
+
+    public Company(String companyName, int employeesNumber) {
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+    }
+
+    public Company(String companyName, int employeesNumber, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employeesNumber = employeesNumber;
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+}
---
+++ src/main/java/com/tw/apistackbase/testEntity/Employee.java
@@ -0,0 +1,51 @@
+package com.tw.apistackbase.entity;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int age;
+    private String gender;
+    private int salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, String gender, int salary) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public Employee(int id, String name, int age, String gender, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
